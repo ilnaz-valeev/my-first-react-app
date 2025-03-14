5abc25f602cae52d23ffef1c95dea36085add3bb
ilnaz valeev <alexsanderkyznecov@mail.ru>
2025-03-14 14:06:05 +0500

    React homework one is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message/Message';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Message text= 'Привет, это текст через props' />
       </header>
     </div>
   );
